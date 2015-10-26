2e6293cff2700091d5dc174222580899806ce7d1
baigt <baiguangtong@gmail.com>
2015-10-26 13:00:31 +0800

    Initialized the project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CardsUnion::Application.config.secret_token = 'f840aad2fdb6f9ee4fe345a0bdbb680a36ad9279db97d969e5ef07d23b5ee6073de5a4779a271d61e6bf740fecc28d7f956565830b8d10ff430dfe994ad68743'
