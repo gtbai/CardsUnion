5a490bb323bed3f22295d3eca1807ac2ebf2a9d8
wzr <m15636335259@163.com>
2015-11-19 20:10:45 +0800

    Search Finished

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,18 @@
+class SearchController < ApplicationController
+	def index
+		  @search = Merchant.search(params[:search])
+		  @merchants = @search.all   # load all matching records
+
+
+		  # @articles = @search.relation # Retrieve the relation, to lazy-load in view
+		  # @articles = @search.paginate(:page => params[:page]) # Who doesn't love will_paginate?
+
+	end
+	end
+	def new
+	end
+	def create
+
+	def destroy
+	end
+end
