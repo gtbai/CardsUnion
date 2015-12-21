c71ba0b8504675088218d2bf06fec53c4fa9d27d
wzr <398359810@qq.com>
2015-12-21 21:26:04 +0800

    John fix a bug that cannot reset password

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -14,7 +14,7 @@ class PasswordResetsController < ApplicationController
   if @account.password_reset_sent_at < 2.hours.ago
     redirect_to new_password_reset_path, :alert => "Password &crarr; 
       reset has expired."
-  elsif @account.update_attribute(:password, params[:account])
+  elsif @account.update_attribute(:password, params[:account][:password])
     redirect_to root_url, :notice => "Password has been reset."
   else
     render :edit
