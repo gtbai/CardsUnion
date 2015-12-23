f69fe5ead1f6bc24e8adf492f0d72d068923c519
HIT-BGT <baiguangtong@gmail.com>
2015-12-24 01:20:52 +0800

    Try sending mail 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,4 +65,7 @@ CardsUnion::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # set host (domain name)
+  config.action_mailer.default_url_options = { :host => "cardsunion.herokuapp.com" }
 end
