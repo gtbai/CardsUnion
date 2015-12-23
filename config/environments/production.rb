c14359e45befd3bec25472a5c5a388db0776839b
HIT-BGT <baiguangtong@gmail.com>
2015-12-23 16:12:09 +0800

    Try precompile2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ CardsUnion::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   # Dont'know what to do!
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
